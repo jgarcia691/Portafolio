9228bc4602a7fd0aa634e1b571cb2926fe6fa6f6
jgarcia691 <jagarciar.info@gmail.com>
2025-06-07 21:23:33 -0400

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Portafolio Personal
+      </h1>
+      <p>Hola, soy un desarrollador web apasionado por crear experiencias digitales únicas. Aquí encontrarás una muestra de mis proyectos y habilidades.</p>
+
+    </>
+  )
+}
+
+export default App
